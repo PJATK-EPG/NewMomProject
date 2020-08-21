1e93f55b332ee4bb6fddf9c64b706d5bb9767f75
ruslan <valuevruslan1999@gmail.com>
2020-08-21 16:14:09 +0200

    Add basic select logic

---
+++ Assets/Scripts/StageZone.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageZone : MonoBehaviour, ISelectable
+{
+    [SerializeField] private Material defaultMaterial;
+    [SerializeField] private Material selectedMaterial;
+
+    private MeshRenderer renderer;
+    private void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+    }
+    public void OnSelected()
+    {
+        renderer.material = selectedMaterial;
+    }
+    public void OnDeselected()
+    {
+        renderer.material = defaultMaterial;
+    }
+
+}
