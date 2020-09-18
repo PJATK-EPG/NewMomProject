d5ff0b0ec08b22315597b151265763c7b515a95c
Ivan <45919772+ivanMuzyka144@users.noreply.github.com>
2020-09-18 21:19:51 +0300

    Create Button Mover

---
+++ Assets/Scripts/MoveableObjects/MeshButton/MeshButton.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshButton : MonoBehaviour, ISelectable
+{
+    [SerializeField] private bool shouldReturn;
+
+    [SerializeField] private Material selectedMaterial;
+    [SerializeField] private Material defaultMaterial;
+    
+
+    private MeshRenderer renderer;
+    private Animator animator;
+
+    private void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    public void OnSelected()
+    {
+        renderer.material = selectedMaterial;
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (shouldReturn)
+            {
+                StartCoroutine(AnimateReturnClick(0.75f));
+            }
+            else
+            {
+                animator.SetTrigger("RegularClick");
+            }
+           
+        }
+    }
+    public void OnDeselected()
+    {
+        renderer.material = defaultMaterial;
+    }
+
+    private IEnumerator AnimateReturnClick(float animationLength)
+    {
+        animator.SetTrigger("ReturnClick");
+
+       yield return new WaitForSeconds(animationLength);
+
+        animator.ResetTrigger("ReturnClick");
+    }
+}
---
+++ Assets/Scripts/MoveableObjects/MeshButton/MeshButtonInformer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+#if (UNITY_EDITOR)
+using UnityEditor;
+#endif
+public class MeshButtonInformer : MonoBehaviour
+{
+#if (UNITY_EDITOR)
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(gameObject.transform.position + new Vector3(0, 0, 0.25f), gameObject.transform.position + new Vector3(0, 0, -0.25f));
+        Gizmos.DrawLine(gameObject.transform.position + new Vector3(0, 0.1f, 0.25f), gameObject.transform.position + new Vector3(0, 0.1f, -0.25f));
+        Gizmos.DrawLine(gameObject.transform.position + new Vector3(0, -0.1f, 0.25f), gameObject.transform.position + new Vector3(0, -0.1f, -0.25f));
+    }
+#endif
+}
