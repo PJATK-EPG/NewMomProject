8e1fbfdde9a75df2e2830c1d4a5e6927642157a3
Ivan <45919772+ivanMuzyka144@users.noreply.github.com>
2020-09-18 16:59:18 +0300

    Creat wheel, improv lever

--- Assets/Scripts/MoveableObjects/Lever/LeverMover.cs
+++ Assets/Scripts/MoveableObjects/Lever/LeverMover.cs
@@ -9,7 +9,14 @@ public class LeverMover : MonoBehaviour, ISelectable
 
     [SerializeField] private Transform centerPoint;
 
+
+    [SerializeField] private float minAngleValue;
+    [SerializeField] private float maxAngleValue;
+    private float midValue;
+
     private MeshRenderer renderer;
+
+    private float leverBackSpeed = 6;
 
     private Vector3 screenPoint;
     private Vector3 offset;
@@ -18,6 +25,7 @@ public class LeverMover : MonoBehaviour, ISelectable
     private void Start()
     {
         renderer = GetComponent<MeshRenderer>();
+        midValue = maxAngleValue / 2;
     }
 
     public void OnSelected()
@@ -42,25 +50,47 @@ public class LeverMover : MonoBehaviour, ISelectable
 
     private void Update()
     {
+        if (!Input.GetMouseButton(0))
+        {
+            AnimateBackMovement();
+        }
         if (Input.GetMouseButton(0) && canMove)
         {
             Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-
-
             Vector3 cursorVector = curScreenPoint;
             Vector3 headVector = Camera.main.WorldToScreenPoint(gameObject.transform.position);
            
             float velocity = cursorVector.y - headVector.y;
-            Debug.Log("cursorVector: "+ cursorVector+ " headVector: "+ headVector);
-            centerPoint.RotateAround(centerPoint.position, centerPoint.up, -1 * velocity * Time.deltaTime);
-        }
-        if (Input.GetMouseButtonUp(0))
+            float yAngle = centerPoint.localEulerAngles.y;
+            yAngle = (yAngle > 200) ? yAngle - 360 : yAngle;
+
+            if(velocity <0 && yAngle < maxAngleValue || velocity > 0 && yAngle > minAngleValue)
+                centerPoint.RotateAround(centerPoint.position, centerPoint.up, (-1)* velocity * Time.deltaTime);
+            
+        }else if (Input.GetMouseButtonUp(0))
         {
             canMove = false;
         }
-        if (!Input.GetMouseButton(0))
-        {
+    }
 
+    public void AnimateBackMovement()
+    {
+        float yAngle = centerPoint.localEulerAngles.y;
+        yAngle = (yAngle > 200) ? yAngle - 360 : yAngle;
+        if (yAngle <= midValue)
+        {
+            if (Quaternion.Angle(centerPoint.transform.localRotation, Quaternion.Euler(0, minAngleValue, 0)) > 0.1f)
+            {
+                centerPoint.transform.localRotation = Quaternion.Lerp(centerPoint.transform.localRotation, Quaternion.Euler(0, minAngleValue, 0), leverBackSpeed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            if (Quaternion.Angle(centerPoint.transform.localRotation, Quaternion.Euler(0, maxAngleValue, 0)) > 0.1f)
+            {
+                centerPoint.transform.localRotation = Quaternion.Lerp(centerPoint.transform.localRotation, Quaternion.Euler(0, maxAngleValue, 0), leverBackSpeed * Time.deltaTime);
+            }
         }
     }
+   
 }
