edfeea86bd04eb56f38a8195ddd16f53751493ab
Ivan <45919772+ivanMuzyka144@users.noreply.github.com>
2020-09-22 12:51:28 +0300

    Change FaderMover

--- Assets/Scripts/MoveableObjects/Fader/FaderMover.cs
+++ Assets/Scripts/MoveableObjects/Fader/FaderMover.cs
@@ -7,6 +7,10 @@ public class FaderMover : MonoBehaviour, ISelectable
     [SerializeField] private Material selectedMaterial;
     [SerializeField] private Material defaultMaterial;
 
+    [SerializeField] private Transform minPoint;
+    [SerializeField] private Transform maxPoint;
+
+
     private MeshRenderer renderer;
 
     private Vector3 maxValue;
@@ -51,17 +55,57 @@ public class FaderMover : MonoBehaviour, ISelectable
     {
         if (Input.GetMouseButton(0) && canMove)
         {
-            Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+            //Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
 
-            Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
-            curPosition = new Vector3(minValue.x, Mathf.Clamp(curPosition.y, minValue.y, maxValue.y), minValue.z);
-            transform.position = Vector3.Lerp(transform.position, curPosition, 7*Time.deltaTime);
+            //Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+
+            //curPosition = new Vector3(minValue.x,
+            //                        Mathf.Clamp(curPosition.y, minValue.y, maxValue.y),
+            //                        minValue.z);
+
+
+            ////Vector3 nearestPoint = FindNearestPointOnLine(curPosition , minPoint.position, maxPoint.position);
+            //transform.position = Vector3.Lerp(transform.position, curPosition, 7*Time.deltaTime);
+
+
+
+            Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+            Vector3 cursorVector = curScreenPoint;
+            Vector3 headVector = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+
+            float velocity = cursorVector.y - headVector.y;
+
+            if (velocity > 0)
+            {
+                transform.position = Vector3.Lerp(transform.position, minPoint.position, 2 * Time.deltaTime);
+            }
+            else
+            {
+                transform.position = Vector3.Lerp(transform.position, maxPoint.position, 2 * Time.deltaTime);
+            }
         }
         if (Input.GetMouseButtonUp(0))
         {
             canMove = false;
         }
 
+    }
+
+    public Vector3 FindNearestPointOnLine(Vector3 origin, Vector3 minPoint, Vector3 maxPoint)
+    {
+        Vector3 p1 = minPoint;
+        Vector3 p2 = maxPoint;
+        Vector3 q = origin;
+
+        Vector3 u = p2 - p1;
+        Vector3 pq = q - p1;
+
+        //Vector3 w1 = new Vector3 
+        Vector3 w2 = pq - u * Vector3.Dot(pq, u)  / u.sqrMagnitude;
+
+        
+        Vector3 point = q - w2;
+        return point;
     }
 }
 
