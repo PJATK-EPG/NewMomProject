a93907ebb5bb855a794986282c834459186c2034
Ivan <45919772+ivanMuzyka144@users.noreply.github.com>
2020-10-11 19:58:13 +0300

    Add listener logic

---
+++ Assets/Scripts/Waiters/WoodenBoxPuzzle/PianoKeyListener.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PianoNotes
+{
+    DO,
+    RE,
+    MI,
+    FA,
+    SOL,
+    LA,
+    SI,
+    ANOTHER
+}
+public class PianoKeyListener : MonoBehaviour
+{
+    public static PianoKeyListener Instance { get; private set; }
+
+    private Queue<PianoNotes> playedNotes = new Queue<PianoNotes>();
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    
+    public void AddNote(PianoNotes note)
+    {
+        playedNotes.Enqueue(note);
+
+        if (playedNotes.Count > 3)
+        {
+            playedNotes.Dequeue();
+        }
+
+        if(playedNotes.Count == 3)
+        {
+            CheckSequence();
+        }
+    }
+
+    private void CheckSequence()
+    {
+        PianoNotes[] lastPlayedNotes = playedNotes.ToArray();
+        if(lastPlayedNotes[0] == PianoNotes.MI && lastPlayedNotes[0] == PianoNotes.SOL && lastPlayedNotes[0] == PianoNotes.SI)
+        {
+            Debug.Log("Activated!!!!");
+        }
+    }
+}
